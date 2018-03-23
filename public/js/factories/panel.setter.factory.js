2027629dfc62b6db329cbf0fad6a81e782b36647
Simon Bor <simon.bor@taravault.net>
2018-03-23 09:48:18 -0400

    refactors to client side only based addon

--- public/js/factories/panel.setter.factory.js
+++
@@ -1,9 +0,0 @@
-angular.module("datasetter", []).factory("datasetter", function() {
-    return {
-        set: function(response){
-            var notRun = response.data.CoverageCountTotal - (response.data.CoverageCountPassed + response.data.CoverageCountFailed + response.data.CoverageCountCaution + response.data.CoverageCountBlocked)
-            return [response.data.CoverageCountFailed, response.data.CoverageCountPassed, notRun, response.data.CoverageCountBlocked, response.data.CoverageCountCaution]
-        }
-
-    }
-});
